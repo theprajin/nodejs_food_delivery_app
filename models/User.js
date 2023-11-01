50621c1504ae56adc175d90522a69ae0be4785b6
theprajin <prazenthekiller@gmail.com>
2023-11-01 13:46:23 +0545

    User model added

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Please provide first name"],
+    minlength: 2,
+    maxlength: 50,
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please provide last name"],
+    minlength: 2,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+    maxlength: 500,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user", "restaurantOwner", "driver"],
+    default: "user",
+  },
+});
+
+UserSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
+
+module.exports = mongoose.model("User", UserSchema);
