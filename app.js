fe7bdecc937d24c64655bc574c18b46c2008df2a
theprajin <prazenthekiller@gmail.com>
2023-11-01 13:35:50 +0545

    database connection added

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+require("dotenv").config();
+require("express-async-errors");
+
+//express
 const express = require("express");
 const app = express();
 
 //rest of the packages
 const morgan = require("morgan");
+
+//routers
+
+//database
+const connectDB = require("./db/connect");
 
 //middleware
 app.use(morgan("tiny"));
@@ -12,4 +21,10 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log(`Server is listening on port: ${port}`));
+const start = async () => {
+  await connectDB(process.env.MONGO_URL);
+  console.log("Connected to the database successfuly");
+  app.listen(port, console.log(`Server is listening on port: ${port}`));
+};
+
+start();
